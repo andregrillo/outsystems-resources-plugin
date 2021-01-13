4b284de3b17ed2b755d8a41e1ee94ce09152c72f
André Grillo <andregrillo@gmail.com>
2021-01-13 13:00:08 +0000

    improved checking if file does not exist

--- src/ios/OutSystemsResources.m
+++ src/ios/OutSystemsResources.m
@@ -15,32 +15,39 @@
 {
     CDVPluginResult* pluginResult = nil;
     NSString* file = [command.arguments objectAtIndex:0];
-
+    
     if (file != nil && [file length] > 0) {
-
+        
         NSString *fileExt = [file pathExtension];
         NSString *fileName = [file stringByDeletingPathExtension];
-	    NSFileManager *fileManager = [NSFileManager defaultManager];
-	    NSError *error;
-	    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-	    NSString *documentsDirectory = [paths objectAtIndex:0];
-	    NSString *docDirFilePath = [documentsDirectory stringByAppendingPathComponent:file];
-
-        if ([fileManager fileExistsAtPath:docDirFilePath] == NO)
-        {
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        NSError *error;
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSString *documentsDirectory = [paths objectAtIndex:0];
+        NSString *docDirFilePath = [documentsDirectory stringByAppendingPathComponent:file];
+        BOOL fileExists = [fileManager fileExistsAtPath:docDirFilePath];
+        
+        if (fileExists) {
             NSString *resourcePath = [[NSBundle mainBundle] pathForResource:fileName ofType:fileExt];
-            [fileManager copyItemAtPath:resourcePath toPath:docDirFilePath error:&error];
-            if (error) {
-                NSLog(@"Error on copying file: %@\nfrom path: %@\ntoPath: %@", error, resourcePath, docDirFilePath);
-                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
-            } 
-	    }					
-
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:file];
+            if (resourcePath != nil && [resourcePath length] > 0) {
+                [fileManager copyItemAtPath:resourcePath toPath:docDirFilePath error:&error];
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:file];
+                if (error) {
+                    NSLog(@"ERROR: Error on copying file: %@\nfrom path: %@\ntoPath: %@", error, resourcePath, docDirFilePath);
+                    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
+                }
+            } else {
+                NSLog(@"ERROR: Invalid file name or file already exists");
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"ERROR: Invalid file name or file already exists"];
+            }
+        } else {
+            NSLog(@"ERROR: File does not exist");
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"ERROR: File does not exist"];
+        }
     } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"File name parameter cannot be null or an empty string"];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
-
+    
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
