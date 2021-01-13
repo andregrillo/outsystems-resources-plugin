b3d9a0180088e41e4001e0ca25703c95f9c21249
André Grillo <andregrillo@gmail.com>
2021-01-13 12:28:03 +0000

    file name as parameter implemented

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # outsystems-resources-plugin
 Enables the use of files added as resources in an OutSystems app
+
+
+'''
+cordova.plugins.OutSystemsResources.getFile("outsystems.png",function(success){},function(error){});
+'''
--- src/ios/OutSystemsResources.m
+++ src/ios/OutSystemsResources.m
@@ -14,29 +14,29 @@
 - (void)getFile:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
-    NSString* fileName = [command.arguments objectAtIndex:0];
+    NSString* file = [command.arguments objectAtIndex:0];
 
-    if (fileName != nil && [fileName length] > 0) {
+    if (file != nil && [file length] > 0) {
 
-    	NSString *openFile ;
+        NSString *fileExt = [file pathExtension];
+        NSString *fileName = [file stringByDeletingPathExtension];
 	    NSFileManager *fileManager = [NSFileManager defaultManager];
 	    NSError *error;
 	    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
 	    NSString *documentsDirectory = [paths objectAtIndex:0];
+	    NSString *docDirFilePath = [documentsDirectory stringByAppendingPathComponent:file];
 
-	        NSString *docDirFilePath = [documentsDirectory stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.png", @"outsystems"]];
-
-	        if ([fileManager fileExistsAtPath:docDirFilePath] == NO)
-	        {
-	            NSString *resourcePath = [[NSBundle mainBundle] pathForResource:@"outsystems" ofType:@"png"];
-	            [fileManager copyItemAtPath:resourcePath toPath:docDirFilePath error:&error];
-	            if (error) {
-	                NSLog(@"Error on copying file: %@\nfrom path: %@\ntoPath: %@", error, resourcePath, docDirFilePath);
-	                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
-	            } 
+        if ([fileManager fileExistsAtPath:docDirFilePath] == NO)
+        {
+            NSString *resourcePath = [[NSBundle mainBundle] pathForResource:fileName ofType:fileExt];
+            [fileManager copyItemAtPath:resourcePath toPath:docDirFilePath error:&error];
+            if (error) {
+                NSLog(@"Error on copying file: %@\nfrom path: %@\ntoPath: %@", error, resourcePath, docDirFilePath);
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
+            } 
 	    }					
 
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:fileName];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:file];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
